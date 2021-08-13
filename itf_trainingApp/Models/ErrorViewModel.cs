3566cf74e8ad9d944170e689993b839ed22a0067
Damilola Adesanya <dami.d.adesanya@gmail.com>
2021-08-13 14:06:39 +0100

    Add project files.

---
+++ itf_trainingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace itf_trainingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
